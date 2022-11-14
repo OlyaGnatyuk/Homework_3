603bb4f7f278e6b0d6fcbe33c2f896c51db080ed
OlyaGnatyuk <43353123+OlyaGnatyuk@users.noreply.github.com>
2022-11-14 21:09:55 +0300

    Add additional checks for simple numbers

--- src/main/java/org/example/simple_number/SimpleNumber.java
+++ src/main/java/org/example/simple_number/SimpleNumber.java
@@ -2,15 +2,21 @@ package org.example.simple_number;
 
 public class SimpleNumber {
     public static void checkIfNumberIsSimple(int number) {
+        if (number < 1) {
+            System.out.printf("%s is not natural number\n", number);
+            return;
+        }
+
+        if (number == 1) {
+            System.out.println("1 is both not simple and not composite number");
+            return;
+        }
+
         boolean detectSimpleNumber = isSimpleNumber(number);
-        System.out.printf("Number %s is %ssimple%n", number, detectSimpleNumber ? "" : "not ");
+        System.out.printf("Number %s is %ssimple\n", number, detectSimpleNumber ? "" : "not ");
     }
 
     private static boolean isSimpleNumber(int number) {
-        if (number < 2) {
-            return false;
-        }
-
         boolean isSimple = true;
         for (int i = number - 1; i > 1; i--) {
             if (number % i == 0) {
